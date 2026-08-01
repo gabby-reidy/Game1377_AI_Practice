19766f6878b877cab1cf4e377fa1d0a9f31a1003
gabby-reidy <gabrielle.reidy@g.austincc.edu>
2026-08-01 13:02:08 -0500

    Updated playercontroller.cs
    implemented click to move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,47 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent agent;
 
+    public InputSystem_Actions InputSystemActions;
+    private InputSystem_Actions.PlayerActions playerActions;
+
+    private Vector3 targetPosition;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        InputSystemActions = new InputSystem_Actions();
+        playerActions = InputSystemActions.Player;
+
+        agent = GetComponent<NavMeshAgent>();
+        targetPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnEnable()
+    {
+        playerActions.Click.performed += ClickToMove;
+        playerActions.Enable();
+    }
+
+    private void ClickToMove(InputAction.CallbackContext context)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            targetPosition = hit.point;
+            agent.SetDestination(targetPosition);
+        }
     }
 }
