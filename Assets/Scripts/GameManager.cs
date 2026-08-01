06228e9564836c40bed00a09c667b5c1f3d036c8
gabby-reidy <gabrielle.reidy@g.austincc.edu>
2026-08-01 13:09:19 -0500

    Set up gamemanager.cs
    Implemented singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+
+    public bool hasKey;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        Instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ResetPlayerToStart()
+    {
+
     }
 }
